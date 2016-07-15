e59e60cb9d85f15054a35b30285892e12a4a5f7c
IOS-开发机 <IOS-fzr@fuzhaoruidemacbook-pro.local>
2016-07-15 16:33:36 +0800

    Initial Commit

---
+++ shengchengerwei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shengchengerwei
+//
+//  Created by IOS-开发机 on 16/7/15.
+//  Copyright © 2016年 IOS-开发机. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shengchengerwei/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shengchengerwei
+//
+//  Created by IOS-开发机 on 16/7/15.
+//  Copyright © 2016年 IOS-开发机. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
